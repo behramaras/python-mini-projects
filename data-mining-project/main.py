177d72a7e3030b88aa3a88f7a899f4d96133278e
Behram Aras <behram.aras@gmail.com>
2025-12-12 10:19:59 +0000

    Added required libraries to data-mining-project/main.py

--- data-mining-project/main.py
+++ data-mining-project/main.py
@@ -1,1 +1,12 @@
-
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (
+    confusion_matrix, roc_auc_score, roc_curve,
+    accuracy_score, precision_score, recall_score, f1_score
+)
+from sklearn.preprocessing import label_binarize
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
