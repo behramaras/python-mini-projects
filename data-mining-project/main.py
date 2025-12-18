08706fa3930bc1e7ed3f4ac95a6f6b338389adf9
Behram Aras <behram.aras@gmail.com>
2025-12-18 10:20:30 +0000

    Add random forest

--- data-mining-project/main.py
+++ data-mining-project/main.py
@@ -130,3 +130,21 @@ plt.title("Sayısal Özellikler Arası Korelasyon Matrisi")
 plt.tight_layout()
 plt.show()
 
+# ==============================================
+# MODELLEME – RANDOM FOREST
+# ==============================================
+
+# Bağımsız değişkenler (X) ve hedef değişken (y) ayırıyoruz
+X = df.drop(['ExamAverage', 'PlacementLevel'], axis=1)  # ExamAverage ve PlacementLevel'ı çıkarıyoruz
+y = df['PlacementLevel']  # Tahmin etmek istediğimiz değişken
+
+# Kategorik değişkenleri one-hot encoding değişkenlere çeviriyoruz
+X = pd.get_dummies(X, columns=['Gender', 'Previous Curriculum (17/18)2'], drop_first=True)
+
+# Veriyi eğitim (%70), doğrulama (%10) ve test (%20) olarak ayırıyoruz
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=2/3, random_state=42, stratify=y_temp)
+
+# Sınıf isimlerini alıyoruz (High, Low, Medium)
+class_names = np.unique(y)
+
